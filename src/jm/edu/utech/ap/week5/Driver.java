2f1b48ad496d18f2366d5b3854e8f2f31ba9c3af
JovielPerkins <jovielperkins@gmail.com>
2018-09-27 14:44:55 -0500

    Driver updated Backspaced

--- src/jm/edu/utech/ap/week5/Driver.java
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -4,7 +4,7 @@ public class Driver implements Drivable{
 	
 	public void drive(float numberOfMiles)
 	{
-		System.out.println(String.format("Jovi has Driven" + "%.2f miles", numberOfMiles));
+	System.out.println(String.format("Jovi has Driven" + "%.2f miles", numberOfMiles));
 	}
 	
 
